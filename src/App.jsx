a0b9ea1f6884acf3dcee26b00c4c0060c275baeb
Asif <beingasifk07@gmail.com>
2024-05-19 01:49:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("black");
+  return (
+    <div className="w-full h-screen " style={{ backgroundColor: color }}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-xl  px-3 py-2 rounded-2xl">
+          <button
+            onClick={() => setColor("black")}
+            className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+          >
+            Dark
+          </button>
+          <button
+            onClick={() => setColor("white")}
+            type="button"
+            className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+          >
+            Light
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            type="button"
+            className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("red")}
+            type="button"
+            className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("yellow")}
+            type="button"
+            className="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900"
+          >
+            Yellow
+          </button>
+          <button
+            onClick={() => setColor("purple")}
+            type="button"
+            className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+          >
+            Purple
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
